a320ad28cc6eb03d7013527c778f90ada0a74401
Son <binlabum123@gmail.com>
2023-11-15 18:29:29 +1030

    initial tests

--- test/test_image_access.py
+++ test/test_image_access.py
@@ -2,12 +2,8 @@ import pytest
 from image_access import ImageAccess
 
 class TestImageAccess:
-    def test_find_matching_image():
-        engine = MatchingEngine()
-        result = engine.findMatchingImage(None)  # Replace None with a mock Image object
-        assert isinstance(result, list)
-
-    def test_compute_cosine_similarity():
-        engine = MatchingEngine()
-        similarity = engine.computeCosineSimilarity(None, None)  # Replace None with mock Image objects
-        assert isinstance(similarity, float)
+    def test_read_image_from_file_system():
+        image_access = ImageAccess()
+        # Assuming readImageFromFileSystem returns an Image object
+        image = image_access.readImageFromFileSystem("path/to/image")
+        assert isinstance(image, Image)  # Replace 'Image' with the actual class/type of the returned object
--- test/test_object_detection_engine.py
+++ test/test_object_detection_engine.py
@@ -10,18 +10,20 @@ class TestObjectDetectionEngine:
 
     def test_analyze_image():
         engine = ObjectDetectionEngine()
-        test_image = None  # Replace with a suitable test image object or path
+        test_image = "path/to/test/image.jpg"  # Replace with a suitable test image path
         engine.analyzeImage(test_image)
         assert isinstance(engine.detectedObjects, list)
         # Additional assertions can be added based on the expected output
+
     def test_analyze_image_with_different_inputs():
         engine = ObjectDetectionEngine()
-        for test_image in test_images:  # Replace with a list of different test images
+        test_images = ["path/to/test/image1.jpg", "path/to/test/image2.jpg"]  # List of test image paths
+        for test_image in test_images:
             engine.analyzeImage(test_image)
             assert isinstance(engine.detectedObjects, list)
             # Additional assertions for each test image
 
     def test_invalid_input():
         engine = ObjectDetectionEngine()
-        with pytest.raises(SomeExpectedException):  # Replace with the expected exception
+        with pytest.raises(Exception):  # Replace with the expected exception once known
             engine.analyzeImage(None)  # Testing with None as an invalid input
